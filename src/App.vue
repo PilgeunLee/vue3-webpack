8d8107c3974c758def9d760780d2576cbb247799
pgsto <pgstom9015@gmail.com>
2024-12-30 23:22:50 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ massege }}</h1>
+    <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+    components:{
+        HelloWorld
+    },
+
+    data(){
+        return {
+            massege:'Hello vue!!!'
+        }
+    }
+}
+</script>
